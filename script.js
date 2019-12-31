6706c7a3aa0aecd75574c1187a24fd3b1b0c2b69
Vibhor Sharma <vibhor5000@gmail.com>
2019-12-31 17:19:20 +0530

    adding html & js files for testing

---
+++ script.js
@@ -0,0 +1,5 @@
+var date = new Date();
+document.body.innerHTML = "<h1> Today is: " +date + "</h1>";
+alert("why the \"fuck are you not understanding\"")
+var a = 9000, b = 90000, c = a*b;
+alert(c);
